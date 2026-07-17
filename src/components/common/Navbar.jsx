221c794cb2b44b20d942d97fb6b8eac5f3a44c12
Thiranya <thiranyasithumini@gmail.com>
2026-07-17 19:39:04 +0530

    Added style into the navbar section.

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,8 +1,19 @@
+import {NavLink} from "react-router";
+
 const Navbar = () => {
+
+    const activeNavButton = () => {
+        return "text-white"
+    };
     return (
-        <div>
-            navbar
-        </div>
+        <nav className="flex items-center justify-between px-6 py-4 ">
+            <span className="text-white text-2xl font-bold">🌥 Weather App</span>
+            <div className="flex gap-2">
+                { /*button that we need to include in here*/}
+                <NavLink to="/" className={activeNavButton}>Home</NavLink>
+                <NavLink to="/About" className={activeNavButton}>About</NavLink>
+            </div>
+        </nav>
     );
 };
 
